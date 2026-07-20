ccff7b7a2d262e56be1977fabf145661e2705f9d
Gan Long <long.gan@scania.com.cn>
2026-07-20 14:13:16 +0800

    update

--- IVI/launcher/Application/generate_cmake_vs2022_solution.bat
+++ IVI/launcher/Application/generate_cmake_vs2022_solution.bat
@@ -1,15 +1,2 @@
-@echo off
-REM Generate VS2022 solution using MSVC toolset v142 (VS2019), matching Kanzi Engine GL_vs2019_* DLLs.
-REM
-REM Expected env (keep as-is if already set):
-REM   Kanzi_DIR  = ...\KanziWorkspace_*\Engine\lib\cmake\Kanzi
-REM   KANZI_HOME = Studio install (e.g. D:\Kanzi 3_9_15_83) for system jars
-
-setlocal
-cd /d "%~dp0"
-
-cmake -S . -B build_vs2022 -G "Visual Studio 17 2022" -A x64 -T v142 %*
-if errorlevel 1 exit /b 1
-echo.
-echo Open: %cd%\build_vs2022\launcher.sln
-endlocal
+cmake -S . -B build_vs2022 -G "Visual Studio 17 2022" -A x64 -T v142
+pause
